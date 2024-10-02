1843669bc5b6980a8eb9cb4684309227ae2fdd07
Caio Carvalho <caio.cmalves@gmail.com>
2024-10-02 10:35:38 -0300

    resolução do video de datacollet resident evil

--- ResidentEvil/collect.py
+++ ResidentEvil/collect.py
@@ -1,40 +1,68 @@
 # %%
 
 import requests
-
-url = 'https://www.residentevildatabase.com/personagens/ada-wong/'
-
-resp= requests.get(url)
-# %%
-
-resp.status_code
-# %%
-
-resp.text
-# %%
 from bs4 import BeautifulSoup
-
-soup = BeautifulSoup(resp.text)
-
-# %%
-div_page = soup.find("div", class_ = "td-page-content")
-paragrafo = div_page.find_all("p")[1]
+from tqdm import tqdm
+import pandas as pd
 
 # %%
-ems = paragrafo.find_all("em")
+def get_content(url):
+    resp= requests.get(url)
+    return resp
+
+def get_basic_infos(soup):
+    div_page = soup.find("div", class_ = "td-page-content")
+    paragrafo = div_page.find_all("p")[1]
+    ems = paragrafo.find_all("em")
+    data = {}
+    for i in ems:
+        chave, valor = i.text.split(":")
+        chave = chave.strip(" ")
+        data[chave] = valor.strip(" ")
+    return data
+
+def get_aparicoes(soup):
+    lis = (soup.find("div", class_ = "td-page-content")
+                .find("h4")
+                .find_next()
+                .find_all("li"))
+    
+    aparicoes = [i.text for i in lis]
+    return aparicoes
+
+def get_personagem_infos(url):
+    resp = get_content(url)
+    if resp.status_code != 200:
+        print('Não foi possível obter os dados')
+        return {}
+    else:
+        soup = BeautifulSoup(resp.text)
+        data = get_basic_infos(soup)
+        data["Aparicoes"] = get_aparicoes(soup)
+        return data
+
+def get_links():
+    url = 'https://www.residentevildatabase.com/personagens/ada-wong/'
+    resp = requests.get(url)
+    soup_personagens = BeautifulSoup(resp.text)
+    ancoras = (soup_personagens.find("div", class="td-page-content")
+                                .find_all("a"))
+    
+    links = [i["href"] for i in ancoras]
+
+    return links
 
 # %%
-ems[0].text
+links = get_links()
+data = []
+for i in tqdm(links):
+    d = get_personagem_infos(i)
+    d["Link"] = i
+    nome = i.strip("/").split("/")[-1].replace("-"," ").title()
+    d["Nome"] = nome
+    data.append(d)
 
 # %%
-data = {}
-for i in ems:
-    chave, valor = i.text.split(":")
-    chave = chave.strip(" ")
-    data[chave] = valor.strip(" ")
-
-data
-# %%
-
-# 50:32
-# https://www.youtube.com/watch?v=K-bIZt_hSBo&list=PLvlkVRRKOYFSrkOL-Bze-42pTdJIAj0_h&index=3&t=669s&ab_channel=T%C3%A9oMeWhy
+df = pd.DataFrame(data)
+df.to_parquet("dados_re.parquet", index=False)
+df.to_pickle("dados.re_pkl")
